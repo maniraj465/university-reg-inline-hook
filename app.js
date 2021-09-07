74b7e9059328208074d823225e00a39616850c85
maniraj465 <smaniraj15@gmail.com>
2021-09-08 00:19:50 +0530

    code

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const { regInlineHookCommandsMapping, regInlineHookErrorMapping } = require('./r
 const API_GATE_WAY_URL = 'https://cg3q7axbka.execute-api.us-east-1.amazonaws.com/stg?email=';
 
 const app = express();
+
+// CORS
+const EXT_CORS_ALLOW_METHODS = 'GET,POST,OPTIONS,PUT,DELETE';
+const EXT_CORS_ALLOW_HEADERS = 'Content-Type,authorization,origin,accept';
+const ORIGIN = 'http://localhost:8080';
+
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', ORIGIN);
+    res.header('Access-Control-Allow-Methods', EXT_CORS_ALLOW_METHODS);
+    res.header('Access-Control-Allow-Headers', EXT_CORS_ALLOW_HEADERS);
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    } else {
+    next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
@@ -28,7 +47,6 @@ async function getUserAWS(targetUrl, registrationEmail) {
     .then(res => {
         if(res && res.status && res.status === 200) {
             if(res && res.data && res.data.Count && res.data.Count === 0) {
-                console.log('here 1');
                 const exception = {
                     statusCode: 404,
                     message: 'User not found with this email!'
@@ -46,7 +64,6 @@ async function getUserAWS(targetUrl, registrationEmail) {
                 return regInlineHookCommandsMapping(res.data.Items[0]);
             }
         } else {
-            console.log('here 2');
             const exception = {
                 statusCode: 500,
                 message: 'Server unavailable, Please try again!'
